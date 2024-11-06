4ec14554658c3f48141c190ba1284d082f6060f3
Prabhudatta Barik <thexas21@gmail.com>
2024-11-06 11:57:23 +0530

    Added bind() in main

--- proxy_server_with_cache.c
+++ proxy_server_with_cache.c
@@ -44,7 +44,7 @@ int cache_size;
 
 int main(int argc, char* argv[]) {
     int client_socketId, client_len;
-    struct sockaddr server_addr, client_addr;
+    struct sockaddr_in server_addr, client_addr;
     sem_init(&semaphore, 0, MAX_CLIENTS);
 
     pthread_mutex_init(&lock, NULL);
@@ -67,5 +67,45 @@ int main(int argc, char* argv[]) {
         perror("setSockOpt failed \n");
     }
     
-    bzero();
+    bzero((char*)&server_addr, sizeof(server_addr));
+
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(port_number);
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    if(bind(proxy_socketId, (struct sockaddr*)&server_addr, sizeof(server_addr)<0)) {
+        perror("Port is not available");
+        exit(1);
+    }
+    printf("Binding on port %d ...\n", port_number);
+    int listen_status = listen(proxy_socketId);
+    if(listen_status < 0) {
+        perror("Error is listening \n");
+        exit(1);
+    }
+
+    int i = 0;
+    int Connected_socketId[MAX_CLIENTS];
+
+    while(1) {
+        bzero((char*)&client_addr, sizeof(client_addr));
+        client_len = sizeof(client_addr);
+        client_socketId = accept(proxy_socketId, (struct sockaddr*)&client_addr, (socklen_t*)&client_len);
+        if(client_socketId < 0) {
+            printf("Not able to connnect ....\n");
+            exit(1);
+        } else {
+            Connected_socketId[i] = client_socketId;
+        }
+
+        struct sockaddr_in * client_pt = (struct sockaddr_in *)&client_addr;
+        struct in_addr ip_addr = client_pt->sin_addr;
+        char str[INET6_ADDRSTRLEN];
+        inet_ntop(AF_INET, &ip_addr, str, INET6_ADDRSTRLEN);
+        printf("Client is connected with port number %d \n IP address is %s\n", ntohs(client_addr.sin_port), str);
+
+        pthread_create(&tid[i], NULL, thread_fn, (void*)&Connected_socketId[i]);
+        i++;
+    }
+    close(proxy_socketId);
+    return 0;
 }
